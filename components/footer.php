3243b7bd72d4c092ea8139160c5352f898f47cce
mstoakley <mstoakley@pvamu.edu>
2024-04-10 02:39:01 -0500

    footer updated

--- components/footer.php
+++ components/footer.php
@@ -1,36 +1,35 @@
-<footer class="footer">
+footer class="footer">
 
    <section class="grid">
 
       <div class="box">
          <img src="images/email-icon.png" alt="">
          <h3>our email</h3>
-         <a href="mailto:shaikhanas@gmail.com">shaikhanas@gmail.com</a>
-         <a href="mailto:anasbhai@gmail.com">anasbhai@gmail.com</a>
+         <a href="mailto:shaikhanas@gmail.com">buonohouston@gmail.com</a>
       </div>
 
       <div class="box">
          <img src="images/clock-icon.png" alt="">
          <h3>opening hours</h3>
-         <p>00:07am to 00:10pm</p>
+         <p>12:00 pm to 10:00pm</p>
       </div>
 
       <div class="box">
          <img src="images/map-icon.png" alt="">
          <h3>our address</h3>
-         <a href="#">mumbai, india - 400104</a>
+         <a href="#">Houston Texas, 936 PV Ln4</a>
       </div>
 
       <div class="box">
          <img src="images/phone-icon.png" alt="">
          <h3>our number</h3>
          <a href="tel:1234567890">123-456-7890</a>
-         <a href="tel:1112223333">111-222-3333</a>
+         
       </div>
 
    </section>
 
-   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>mr. web designer</span> | all rights reserved!</div>
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Tech Legends</span> | all rights reserved!</div>
 
 </footer>
 
