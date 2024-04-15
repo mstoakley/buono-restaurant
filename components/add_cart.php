e36ba857ba08459a1bc44c871ab057db5456b1b2
mstoakley <mstoakley@pvamu.edu>
2024-04-15 14:47:16 -0500

    found add to cart error and fixed it

--- components/add_cart.php
+++ components/add_cart.php
@@ -18,13 +18,13 @@ if(isset($_POST['add_to_cart'])){
       $qty = filter_var($qty, FILTER_SANITIZE_STRING);
 
       $check_cart_numbers = $conn->prepare("SELECT * FROM `orderitems` WHERE CustomerID = ?");
-      $check_cart_numbers->execute([$name, $user_id]);
+      $check_cart_numbers->execute([$user_id]);
 
       if($check_cart_numbers->rowCount() > 0){
          $message[] = 'already added to cart!';
       }else{
          $insert_cart = $conn->prepare("INSERT INTO `orderitems`(CustomerID, MenuID, Price, Quantity, Image) VALUES(?,?,?,?,?)");
-         $insert_cart->execute([$user_id, $pid, $name, $price, $qty, $image]);
+         $insert_cart->execute([$user_id, $pid,$price, $qty, $image]);
          $message[] = 'added to cart!';
          
       }
